0948d1969369ea732897722fbdcea504033b04cd
Hiren Vaghasiya <hirenvaghasiya256@gmail.com>
2015-12-21 17:00:38 +0530

    Update Que2.php

--- Que2.php
+++ Que2.php
@@ -1,21 +1,20 @@
 <?php
-
-$string = "Hiren is best programmer in the world";
+error_reporting(0);
+$string = "Raj is the best programmer in the world";
 $i = 0;
 $wordArray = array();
+$lenArray = array();
 $cnt =0;
 $preLen = 0;
-
+$totalChar = 0;
 $word = "";
 while($string[$i] != NULL)
 {
     if($string[$i] == " ")
     {
-        $preLen = $i - $preLen - $cnt ;
         $wordArray[$cnt] = $word;
+        $cnt++;
         $word = "";
-       echo $preLen."\n";
-       $cnt++;
     }
     else
     {
@@ -24,7 +23,59 @@ while($string[$i] != NULL)
     $i++;
    
 }
+
 $wordArray[$cnt] = $word;
-print_r($wordArray);    
- echo count($wordArray);
+
+for($i = 0; $i<=$cnt; $i++)
+{
+    for($j=$i; $j<=$cnt;$j++)
+    {
+        if(Len($wordArray[$i]) > Len($wordArray[$j]))
+        {
+            $tmp = $wordArray[$i];
+            $wordArray[$i] = $wordArray[$j];
+            $wordArray[$j] = $tmp;
+        }
+        if(Len($wordArray[$i]) == Len($wordArray[$j]))
+        {
+            if(Cmp($wordArray[$i],$wordArray[$j]) == 1)
+            {
+                $tmp = $wordArray[$j];
+                $wordArray[$j] = $wordArray[$i];
+                $wordArray[$i] = $tmp;
+            }
+        }
+    }
+}
+$i =0;
+while($i <= $cnt)
+{
+    echo $wordArray[$i]."  ";
+    $i++;
+}
+function Len($str)
+{
+    $len = 0;
+    while($str[$len] != NULL)
+    {
+        $len++;
+    }
+    return $len;
+}
+function Cmp($str1, $str2)
+{
+    if($str1 > $str2)
+    {
+        return 1;
+    }
+    else if($str1 < $str2)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 ?>
